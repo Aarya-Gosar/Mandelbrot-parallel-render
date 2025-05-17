2e7dd4846131c75a687e56e6fe2d3f29205ee3de
unknown <aaryag.official@gmail.com>
2025-05-17 19:26:37 +0530

    progress bar

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <complex>
 #include <vector>
+#include <cstdlib>
+#include <chrono>
 
 using namespace std;
 #define print(x) cout << x << endl
@@ -8,9 +10,9 @@ using namespace std;
 #define forj(n) for( int j = 0; j < n;j++)
 #define fore(i, left, right) for(int i = int(left); i < int(right); i++)
 
-#define length  400
-#define scale  2
-#define maxiter 50
+int length =  1000;
+double scale = 2;
+int maxiter  =50;
 
 double map2xy(double i){
 	double point =  i*scale/(length - 1) -  scale*(1 - i/(length - 1));
@@ -49,7 +51,11 @@ void make_img(vector<vector<int>>& img){
 	}
 }
 
-int main(){
+int main(int argc, char* argv[]){
+	auto starttime = chrono::high_resolution_clock::now();
+	if (argc > 1) length = atoi(argv[1]);
+	if (argc > 2) maxiter = atoi(argv[2]);
+
 
 
 	print("P3");
@@ -79,7 +85,19 @@ int main(){
 		}
 	}
 
+	
 	make_img(img);
+
+	auto endtime = chrono::high_resolution_clock::now();
+	chrono::duration<double,milli> duration = endtime -starttime;
+	if(duration.count()  < 2000){
+		clog << "\nTime taken: " <<(duration.count()) << "ms \n";
+	}
+	else{
+		clog << "\nTime taken: " <<(duration.count()/1000) << "s \n";
+	}
+	
+
 
 
 
